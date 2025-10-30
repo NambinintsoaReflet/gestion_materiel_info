06a7921d1048cfedde13b46b50207d3b4d28ecd8
Olivier <nambinintsoaolivier25@gmail.com>
2025-10-30 11:48:42 +0300

    mis à jour login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -107,11 +107,37 @@ const Login = () => {
                     )}
 
                     <button
-                      className="w-full mt-2 px-4 py-2 font-bold cursor-pointer text-white bg-[#4face4] rounded hover:bg-blue-700 dark:bg-blue-700 dark:text-white dark:hover:bg-blue-900 focus:outline-none focus:shadow-outline"
+                      className="w-full mt-2 px-4 py-2 font-bold cursor-pointer text-white bg-[#4face4] rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline flex items-center justify-center"
                       type="submit"
                       disabled={isPending}
                     >
-                      {isPending ? "Connexion..." : "Se connecter"}
+                      {isPending ? (
+                        <>
+                          <svg
+                            className="animate-spin -ml-1 mr-2 h-5 w-5 text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle
+                              className="opacity-25"
+                              cx="12"
+                              cy="12"
+                              r="10"
+                              stroke="currentColor"
+                              strokeWidth="4"
+                            ></circle>
+                            <path
+                              className="opacity-75"
+                              fill="currentColor"
+                              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                          </svg>
+                          Connexion en cours...
+                        </>
+                      ) : (
+                        "Se connecter"
+                      )}
                     </button>
                   </div>
                 </form>
