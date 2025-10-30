9947b61e54fb2c38a196b676935af617a66ecb09
Olivier <nambinintsoaolivier25@gmail.com>
2025-10-30 10:40:53 +0300

    mis en prod

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  //   baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:8000/api",
-  baseURL: "http://localhost:8000/api",
+  //   baseURL: import.meta.env.VITE_API_URL ?? "https://toamalink.alwaysdata.net/gmi/",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://toamalink.alwaysdata.net/gmi/",
   withCredentials: true,
 });
 
